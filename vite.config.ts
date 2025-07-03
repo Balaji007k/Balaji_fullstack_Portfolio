6cff5dca2fe93ae527151d53199cd831155032ec
Balaji007k <k446521@gmail.com>
2025-07-03 18:20:19 +0530

    route change

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
-  base: '/pixel-dream-weave/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
